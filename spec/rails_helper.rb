aa79d5bdcd40e98d3231e70674f7d42c6c01f7a5
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-05 11:40:10 +0900

    スペルミスの修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 #ローカルのエラーメッセージを英語に変更
-I18n.local = "en"
+I18n.locale = "en"
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
