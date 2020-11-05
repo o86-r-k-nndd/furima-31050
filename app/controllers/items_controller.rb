1881e58717be1ff13e7f5e8550eb9314ad0d9a10
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-05 18:31:15 +0900

    仮のストロングパラメーターを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+  private
+
+  #ストロングパラメーターの設定
+  def item_params
+    params.require(:item).permit( :explantion,
+                                  :name,
+                                  :category_id,
+                                  :status_id,
+                                  :shipping_id,
+                                  :delivery_area_id,
+                                  :delivery_days_id,
+                                  :price,
+                                  :image
+                                ).merge(
+                                  user_id: current_user.id
+                                )
 end
