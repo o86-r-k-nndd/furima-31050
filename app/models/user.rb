a10a32e7d395eaaeb28b02c0bbda292bf8c5ef03
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-05 12:35:30 +0900

    レビュー指摘点修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #バリデーションの設定
-  validates :nickname,        presence: true
-  #passwordの文字列の入力時に英数字混合かどうかの制約をかける
+  #バリデーションの設定 not null
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
+  #バリデーションの設定 氏名の入力の際 全角（漢字、ひらがな、カタカナ）入力の制約をかける
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  #バリデーションの設定 氏名のフリガナ入力の際 カタカナでの入力の制約をかける
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  #バリデーションpasswordの文字列の入力時に英数字混合かどうかの制約をかける
   validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ン]+\z/}
-  validates :birthday,        presence: true
+
 end
