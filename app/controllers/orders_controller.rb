5bded89bf2b27a282be5bd5235b14d8eb487c7fa
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-11 16:52:27 +0900

    set orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+
+  # 購入ページ表示
+  def index
+    @order_address = OrderAddoress.new
+  end
+
+  # 購入処理
+  def create
+    @order_address = OrderAddoress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render: action: :index
+    end
+  end
+
+  private
+
+  #paramsに送られてきた入力フォームの情報を取得するストロングパラメーター
+  def order_params
+    params.require(:order_address ).permit( :postal_code,
+                                            :municipalities,
+                                            :address,
+                                            :building_name,
+                                            :phone_number,
+                                            :delivery_area_id
+                                  ).merge(  :user_id: params[item.user.id],
+                                            :item_id: params[item.id] )
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -17,14 +17,19 @@ class OrderAddress
   with_option presence: true do
     validates :municipalities
     validates :address
+    # orderモデルの属性
     validates :user_id
     validates :item_id
+
     # ActivdHash "---"の時は保存できない
     validates :delivery_area_id  numericality: { other_than: 1 }
+
     # 郵便番号はハイフンを含んだ数値
     validates :postal_code,     format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+
     # 電話番号は11桁以内の値
     validates :phone_number,    format: { with: /\A[0-9]{,11}\z/ }
+    
   end
 
   # 複数のテーブルへ保存する処理
