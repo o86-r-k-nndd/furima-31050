3a439c4381e7b975d87b909df1f022dc6fbcb8ed
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-11 20:04:21 +0900

    Formオブジェクトを用いて複数のテーブルに値が保存できた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,33 +2,39 @@ class OrdersController < ApplicationController
 
   # 購入ページ表示
   def index
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new
+    set_action_index
   end
 
   # 購入処理
   def create
+    @order_address = OrderAddress.new(order_params)
     binding.pry
-    # @order_address = OrderAddress.new(order_params)
-    # if @order_address.valid?
-    #   @order_address.save
-    #   redirect_to root_path
-    # else
-       #render action: :index
-    # end
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      set_action_index
+      render action: :index
+    end
   end
 
   private
 
   #paramsに送られてきた入力フォームの情報を取得するストロングパラメーター
-  # def order_params
-  #   params.require(:order_address ).permit( :postal_code,
-  #                                           :municipalities,
-  #                                           :address,
-  #                                           :building_name,
-  #                                           :phone_number,
-  #                                           :delivery_area_id
-  #                                 ).merge(  item_id: params[:item.id] )
-  # end
+  def order_params
+    params.require(:order_address ).permit( :postal_code,
+                                            :municipalities,
+                                            :address,
+                                            :building_name,
+                                            :phone_number,
+                                            :delivery_area_id
+                                  ).merge(  item_id: params[:item_id] )
+  end
+
+  # indexアクションを行う時に呼び出すインスタンスを取得するメソッド
+  def set_action_index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
 
 end
