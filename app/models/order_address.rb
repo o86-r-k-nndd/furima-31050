340203340114682d35aef08ec4c880404d54bb72
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-11 11:50:51 +0900

    バリデーションを設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,22 +1,26 @@
 class OrderAddress
 
-  #Formオブジェクトを定義(バリデーションやメソッドを扱える様にする)
+  # Formオブジェクトを定義(バリデーションやメソッドを扱える様にする)
   include ActiveModel::Model
 
-  #OrdersテーブルとAddressesテーブルのカラムを扱える様に属性値を定義
-  attr_accessor :user_id,
-                :item_id,
-                :postal_code,
+  # OrdersテーブルとAddressesテーブルのカラムを扱える様に属性値を定義
+  attr_accessor :postal_code,
                 :municipalities,
                 :address,
                 :building_name,
                 :phone_number,
-                :prefectures_id,
-                :order_id
+                :prefectures_id
 
-  #OrderモデルとAddressモデルのバリデーションを定義
+  # OrderモデルとAddressモデルのバリデーションを定義
   with_option presence: true do
-    validates :postal_code
+    validates :delivery_area_id
+    validates :municipalities
+    validates :address
+    # 郵便番号はハイフンを含んだ数値
+    validates :postal_code,     format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    # 電話番号は11桁以内の整数の値
+    validates :phone_number,    numericality: { only_integer:           true,
+                                                less_than_or_equal_to:  11 }
   end
 
 end
