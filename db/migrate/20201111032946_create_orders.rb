1f5269f8ca1756afa1adfd7e80e6eed6c03cee5b
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-11 12:37:01 +0900

    g address model

--- db/migrate/20201111032946_create_orders.rb
+++ db/migrate/20201111032946_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.references :user
+      t.references :item
       t.timestamps
     end
   end
