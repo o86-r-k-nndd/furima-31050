e9753e7e06ce87dc5c93c14fa79d8317f69fe248
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-11 11:18:27 +0900

    属性を定義

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,4 +3,12 @@ class OrderAddress
   #Formオブジェクトを定義(バリデーションやメソッドを扱える様にする)
   include ActiveModel::Model
 
+  #OrdersテーブルとAddressesテーブルのカラムを扱える様に属性値を定義
+  attr_accessor :postal_code,
+                :municipalities,
+                :address,
+                :building_name,
+                :phone_number,
+                :prefectures_id
+
 end
