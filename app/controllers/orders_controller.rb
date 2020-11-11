08a55cfd25127c06ba919a4fe782cc4b43a49902
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-11 18:47:19 +0900

    order/index 購入内容表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,20 @@ class OrdersController < ApplicationController
 
   # 購入ページ表示
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   # 購入処理
   def create
-    @order_address = OrderAddress.new(order_params)
-    if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
-    else
-      #render: action: :index
-    end
+    binding.pry
+    # @order_address = OrderAddress.new(order_params)
+    # if @order_address.valid?
+    #   @order_address.save
+    #   redirect_to root_path
+    # else
+    #   render: action: :index
+    # end
   end
 
   private
