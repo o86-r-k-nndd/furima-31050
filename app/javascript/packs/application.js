a122ec71c545062f4a3eb5c8375db799e1f93598
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-12 16:41:47 +0900

    g file card.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ require("channels")
 //手数料を表示するためのJavaScriptファイル
 require("../item_price");
 
+//カード情報を取得するためのJavaScriptファイル
+require("../card");
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
