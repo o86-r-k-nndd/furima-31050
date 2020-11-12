87a3641057510e804fcdf6de2e7294e7c20cd157
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-12 23:25:03 +0900

    環境変数に公開鍵を設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,8 @@
 // クレジットカード決済機能
 const card = () => {
 
+  // 外部API PAY.JPの処理を行うための鍵の設定
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   // 商品購入ページの住所情報、カード情報取得
   const form = document.getElementById("charge-form");
   // 購入するボタンが押された時の処理
@@ -10,6 +12,7 @@ const card = () => {
     e.preventDefault();
 
     console.log("card event ok");
+    console.log(process.env.PAYJP_PUBLIC_KEY);
   });
   // //商品購入ページの住所情報、カード情報取得
 };
---
+++ config/initializers/webpacker.rb
@@ -0,0 +1,2 @@
+# javascriptで環境変数を呼び出せる様にする
+Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
