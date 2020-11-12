87ffc57f57c1b8a87b19783ee722803a5cf8b35c
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-12 15:16:21 +0900

    ファクトリーボットの設定

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :order_address do
-    
+  
+    postal_code {123-4567}
+    municipalities {"市区町村"}
+    address {"番地"}
+    phone_number {"09012345678"}
+    item_id {1}
+    building_name {"建物名"}
+
+    # ActiveHash
+    delivery_area_id {Faker::Number.between( from: 2, to: 48 )}
+
   end
 end
