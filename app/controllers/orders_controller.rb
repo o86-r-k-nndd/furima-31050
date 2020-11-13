6c04b75134f07ddea7b8aca1ac513d1ed7355ace
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-13 10:56:05 +0900

    レビュー指摘点修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,8 @@
 class OrdersController < ApplicationController
-
   # ログインしていない場合はログイン画面へ遷移する処理を行う
   before_action :authenticate_user!
-
   # 出品した商品の購入ページへ出品者が遷移しようとした時トップページへ遷移する処理
   before_action :user_seller?
-
   # 購入済みの商品の購入ページへ遷移しようとした時の処理
   before_action :sold_out?
 
@@ -16,45 +13,38 @@ class OrdersController < ApplicationController
 
   # 購入処理
   def create
-
     @order_address = OrderAddress.new(order_params)
 
-    if form_valid?
+    if @order_address.valid?
       @order_address.save
       pay_item
       redirect_to root_path
-
     else
       set_new_form
-      form_valid?
+      @order_address.valid?
       render action: :index
-      
     end
+
   end
 
   private
 
   #paramsに送られてきた入力フォームの情報を取得するストロングパラメーター
   def order_params
-    params.require(:order_address ).permit( :postal_code,
-                                            :municipalities,
-                                            :address,
-                                            :building_name,
-                                            :phone_number,
-                                            :delivery_area_id
-                                  ).merge(  item_id:  params[:item_id],
-                                            token:    params[:token] )
+    params.require(:order_address).permit(:postal_code,
+                                          :municipalities,
+                                          :address,
+                                          :building_name,
+                                          :phone_number,
+                                          :delivery_area_id
+                                  ).merge(item_id:  params[:item_id],
+                                          token:    params[:token])
   end
 
   # indexアクションを行う時に呼び出すインスタンスを取得するメソッド
   def set_new_form
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-  end
-
-  # OrderAddressモデルのバリデーションが正常に動作しているか確認する
-  def form_valid?
-    @order_address.valid?
   end
 
   # 自分が出品した商品の遷移しようした時トップページへ遷移する処理
@@ -76,7 +66,7 @@ class OrdersController < ApplicationController
   # PAY.JPを用いた購入処理
   def pay_item
     @item_price = Item.find(params[:item_id])
-    
+
     # トークンの送信
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
