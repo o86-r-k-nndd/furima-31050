4048b0887b034b1d48b04f9ddd82bff6ec599a11
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-13 15:23:35 +0900

    ベーシック認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  #Basic認証を行う
+  before_action :basic_auth
   #deviseのコントローラーの処理を実行する前にメソッドの処理を行う
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -13,5 +15,12 @@ class ApplicationController < ActionController::Base
                                                         :last_name_kana,
                                                         :birthday
                                                       ])
+  end
+
+  #ベーシック認証を行う処理
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
