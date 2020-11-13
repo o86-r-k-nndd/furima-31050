4aaa9bee1528b0b46e6e3a0a2466635c1f03ce50
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-13 15:36:08 +0900

    ベーシック認証を環境変数へ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   #ベーシック認証を行う処理
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == ENV["BASIC_AUTH_USER_FURIMA"] && password == ENV["BASIC_AUTH_PASSWORD_FURIMA"]
     end
   end
 end
